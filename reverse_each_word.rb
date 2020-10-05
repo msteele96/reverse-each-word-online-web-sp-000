13a82dcab91f6212fe0c58a093403b5f7a7e591c
Marc Steele <marcsteele2014@gmail.com>
2020-10-05 16:32:35 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+#def reverse_each_word(string)
+#  array = string.split(" ")
+#  reversed = ""
+#    array.each do |word|
+#      if array.index(word) == array.length-1
+#        reversed << word.reverse
+#      else
+#        reversed << word.reverse + " "
+#      end
+#    end
+#  return reversed
+#end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  reversed = ""
+  reversed << (array.collect{|word| word.reverse}).join(" ")
+end
